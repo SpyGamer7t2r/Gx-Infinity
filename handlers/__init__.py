745e8ba41e21de3ba961113b0c2d51740068b8af
༄ᶦᶰᵈ᭄Sᴘʏ✿Gᴀᴍᴇʀ࿐ <gamingwithmayuresh7t@gmail.com>
2025-07-25 02:00:43 +0530

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,12 @@
+# handlers/__init__.py
+
+"""
+This file initializes the handlers module.
+Each handler file should be imported here.
+"""
+
+from .ai_handler import *
+from .admin_handler import *
+from .game_handler import *
+from .media_handler import *
+from .user_handler import *
